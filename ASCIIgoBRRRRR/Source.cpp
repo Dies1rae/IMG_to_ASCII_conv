d29900779613ab9ca9e9a0a136ce80be4e5ac401
Николай Полтавский <unluck.bastard@gmail.com>
2020-07-10 01:24:23 +0300

    some bugs in opencv

--- ASCIIgoBRRRRR/Source.cpp
+++ ASCIIgoBRRRRR/Source.cpp
@@ -7,6 +7,8 @@
 int main(int argc, char* argv[]) {
 	std::string dir_in = "C:\\Users\\n.poltavskiy\\Source\\Repos\\ASCIIgoBRRRRR\\x64\\Release\\in";
 	std::string dir_out = "C:\\Users\\n.poltavskiy\\Source\\Repos\\ASCIIgoBRRRRR\\x64\\Release\\out";
+	//std::string dir_in = "C:\\Users\\Vergilius\\source\\repos\\IMG_to_ASCII_conv\\x64\\Release\\in";
+	//std::string dir_out = "C:\\Users\\Vergilius\\source\\repos\\IMG_to_ASCII_conv\\x64\\Release\\out";
 	std::string mask = "jpg";
 	FS main_serv(dir_in, dir_out, mask);
 	main_serv.run();
